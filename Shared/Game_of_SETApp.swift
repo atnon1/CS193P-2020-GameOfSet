7ebf96b3a95ed1e0f7740573d942fc2aa546cf49
Anton Makeev <at-mak@mail.ru>
2020-12-16 10:39:46 +0300

    Initial Commit

---
+++ Shared/Game_of_SETApp.swift
@@ -0,0 +1,17 @@
+//
+//  Game_of_SETApp.swift
+//  Shared
+//
+//  Created by Anton Makeev on 16.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct Game_of_SETApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
